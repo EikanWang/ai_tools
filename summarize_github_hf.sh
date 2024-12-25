db30c3ae0c5259daac3438cd409428faf90709a6
Wang, Eikan <eikan.wang@intel.com>
2024-12-25 11:45:12 +0000

    Add shell scripts to facilitate summarization

---
+++ summarize_github_hf.sh
@@ -0,0 +1,5 @@
+export ftp_proxy=ftp://proxy-dmz.intel.com:912
+export https_proxy=http://proxy-dmz.intel.com:912
+export socks_proxy=socks://proxy-dmz.intel.com:912
+export http_proxy=http://proxy-dmz.intel.com:912
+GITHUB_TOKEN=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 OPENAI_API_KEY=sk-45d50fff73ce424a92451fbb4c4cfe60 /home/eikan/miniforge3/envs/pyt-xpu/bin/python3 /home/eikan/local_disk/ai_tools/summarize_github.py --combine-summaries --send-email --owner huggingface --repo transformers > /home/eikan/local_disk/ai_tools/summarize_github_hf.log 2>&1
